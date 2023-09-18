9eaf62ace11850c9faa9cf3026197d85e2b53914
Xylar Asay-Davis <xylarstorm@gmail.com>
2023-09-18 13:43:18 +0200

    Print output in tests

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -4,8 +4,8 @@ from datetime import date, datetime, time
 from backports.datetime_fromisoformat import MonkeyPatch
 MonkeyPatch.patch_fromisoformat()
 
-datetime.fromisoformat("2014-01-09T21:48:00-05:30")
+print(datetime.fromisoformat("2014-01-09T21:48:00-05:30"))
 
-date.fromisoformat("2014-01-09")
+print(date.fromisoformat("2014-01-09"))
 
-time.fromisoformat("21:48:00-05:30")
+print(time.fromisoformat("21:48:00-05:30"))
