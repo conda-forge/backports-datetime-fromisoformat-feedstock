7c323d1d43c046405a2461ae25a853fa25c96f31
Xylar Asay-Davis <xylarstorm@gmail.com>
2024-09-07 11:33:32 +0200

    Fix tests for newer python

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
-
+import sys
 from datetime import date, datetime, time
-from backports.datetime_fromisoformat import MonkeyPatch
-MonkeyPatch.patch_fromisoformat()
+
+if sys.version_info <= (3, 11, 0):
+    from backports.datetime_fromisoformat import MonkeyPatch
+    MonkeyPatch.patch_fromisoformat()
 
 print(datetime.fromisoformat("2014-01-09T21:48:00-05:30"))
 
